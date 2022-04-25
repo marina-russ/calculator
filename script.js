dfbd9e591554c3f5767e0b8cb836474424d89ed0
Marina Russ <mrusscode@gmail.com>
2022-04-25 19:54:48 -0400

    adds memory button functionality

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const deleteButton = document.querySelector('[data-delete]');
 const allClearButton = document.querySelector('[data-all-clear]');
 const previousOperandTextElement = document.querySelector('[data-previous-operand]');
 const currentOperandTextElement = document.querySelector('[data-current-operand]');
+//Optional: Memory Functions
+const memorySaveButton = document.querySelector('[data-memory-save]');
+const memoryClearButton = document.querySelector('[data-memory-clear]');
+const memoryRecallButton = document.querySelector('[data-memory-recall]');
+let memoryValue;
 
 //MDN: Classes are a template for creating objects. They encapsulate data with code to work on that data.
 class Calculator {
@@ -12,40 +17,39 @@ class Calculator {
     this.previousOperandTextElement = previousOperandTextElement;
     this.currentOperandTextElement = currentOperandTextElement;
     this.clear();
-    // we call clear() when the calculator is first created, so that all inputs are cleared and set to their default values
-  }
+  };
 
   clear() {
-    this.currentOperand = '';
+    this.currentOperand = '0';
     this.previousOperand = '';
     this.operation = undefined;
-  }
+  };
 
   delete() {
     this.currentOperand = this.currentOperand.toString().slice(0, -1);
-  }
+  };
 
   appendNumber(number) {
     if (number === '.' && this.currentOperand.includes('.')) return;
     // if user presses decimal button but operand already has one, fxn will return instead of appending another decimal
     this.currentOperand = this.currentOperand.toString() + number.toString();
     //converts to string so that Javascript appends the number to the end of the Operand, instead of treating + as addition operation. [typing "1" and "3" gives us "13", not 4, etc.]
-  }
+  };
 
   chooseOperation(operation) {
     if (this.currentOperand === '') {
       return;
       // if the current operand is empty, stop running this fxn
-    }
+    };
     if (this.previousOperand !== '') {
       this.compute();
-      // if previous operand is not empty, run the compution fxn
-    }
+      // if previous operand is not empty, run the computation fxn
+    };
     this.operation = operation;
     this.previousOperand = this.currentOperand;
     //when an operation is clicked, the current operand is 'finished' and is set to be the previous operand
     this.currentOperand = '';
-  }
+  };
 
   compute() {
     let computation;
@@ -54,7 +58,7 @@ class Calculator {
     //converts strings back into numbers
     if (isNaN(prev) || isNaN(current)) {
       return;
-    }
+    };
     switch (this.operation) {
       case '+':
         computation = prev + current;
@@ -70,11 +74,11 @@ class Calculator {
         break;
       default:
         return;
-    }
+    };
     this.currentOperand = computation;
     this.operation = undefined;
     this.previousOperand = '';
-  }
+  };
 
   updateDisplay() {
     this.currentOperandTextElement.innerText = this.currentOperand;
@@ -82,8 +86,22 @@ class Calculator {
       this.previousOperandTextElement.innerText = `${this.previousOperand} ${this.operation}`;
     }
     this.previousOperandTextElement.innerText = this.previousOperand;
-  }
-}
+  };
+
+  memorySave() {
+    this.memoryValue = this.currentOperand;
+    this.updateDisplay();
+  };
+
+  memoryClear() {
+    this.memoryValue = '';
+  };
+
+  memoryRecall() {
+    this.currentOperand = this.memoryValue;
+    this.updateDisplay();
+  };
+};
 
 // With a class, we need to hook up the const variable buttons to the calculator object. This is done by calling new [ClassName]. Then pass everything from the constructor into it.
 const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
@@ -115,4 +133,17 @@ allClearButton.addEventListener('click', button => {
 deleteButton.addEventListener('click', button => {
   calculator.delete();
   calculator.updateDisplay();
+});
+
+//Optional: Memory Functions
+memorySaveButton.addEventListener('click', button => {
+  calculator.memorySave();
+});
+
+memoryClearButton.addEventListener('click', button => {
+  calculator.memoryClear();
+});
+
+memoryRecallButton.addEventListener('click', button => {
+  calculator.memoryRecall();
 });
